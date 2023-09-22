acfba5529f328f9ab7530b132f5fd1289f9bb93e
Qepanna <karh@ign.ku.dk>
2023-09-22 17:28:36 +0200

    update: documentation

--- R/MDF.R
+++ R/MDF.R
@@ -6,9 +6,12 @@
 #' @param p numerical; precision of the instrument (same units as measured gas; ex. ppm)
 #' @param t numerical; measurement time (enclosure time; seconds)
 #' @param n numerical; number of measurements during measurement time
-#' @param flux.term numerical; flux term calculated with the function flux.term
+#' @param flux.term numerical; flux term calculated with the function `flux.term()`
 #'
 #' @return a numerical value
+#'
+#' @references Brechet et al. (2021). Simultaneous tree stem and soil greenhouse gas (CO<sub>2</sub>, CH<sub>4</sub>, N<sub>2</sub>O) flux measurements: a novel design for continuous monitoring towards improving flux estimates and temporal resolution. *New Phytologist*, 230(6), 2487-2500.
+#' @references Christiansen et al. (2015). Comparison of CO<sub>2</sub>, CH<sub>4</sub> and N<sub>2</sub>O soil-atmosphere exchange measured in static chambers with cavity ring-down spectroscopy and gas chromatography. *Agricultural and Forest Meteorology*, 211, 48-57.
 #'
 #' @keywords internal
 #'
--- R/k.max.R
+++ R/k.max.R
@@ -1,15 +1,21 @@
-#' KAPPA max
+#' KAPPA MAX
 #'
-#' The parameter kappa determines the curvature of the non-linear flux in the
-#' Hutchinson and Mosier model. A maximum threshold for this parameter, kappa-max
+#' The parameter kappa determines the curvature of the non-linear regression in the
+#' Hutchinson and Mosier model. A maximum threshold for this parameter, kappa-max,
 #' can be calculated based on the minimal detectable flux (MDF), the linear
-#' flux estimate and the measurement time. The units of the kappa-max is s-1
+#' flux estimate and the measurement time. The unit of the kappa-max is s-1.
 #'
-#' @param MDF numerical; minimal detectable flux, calculated with the function MDF
-#' @param LM.flux numerical; flux estimate from a linear flux calculation model
-#' @param t measurement time (enclosure time) (seconds)
+#' @param MDF numerical; minimal detectable flux, calculated with the function `MDF()`
+#' @param LM.flux numerical; flux estimate from a linear flux calculation model,
+#'                calculated with the function `LM.flux()`.
+#' @param t numerical; measurement time (enclosure time) (seconds)
 #'
 #' @return a numerical value
+#'
+#' @seealso Look up the functions [MDF()] and [LM.flux()] of this package
+#'          for more information about these parameters.
+#'
+#' @references Hüppi et al. (2018). Restricting the nonlinearity parameter in soil greenhouse gas flux calculation for more reliable flux estimates. *PloS one*, 13(7), e0200876.
 #'
 #' @keywords internal
 #'
