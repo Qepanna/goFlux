5a28c5d5150f9556a0e4504e4d536c516135af3f
Qepanna <karh@ign.ku.dk>
2024-05-24 22:24:02 +0200

    register S3 methods

--- R/seq.rep.R
+++ R/seq.rep.R
@@ -1,20 +1,21 @@
 #' Sequence repeat
 #'
-#' A wrapper function that merges seq() and rep().
+#' A wrapper function that merges \code{\link[base]{seq}} and \code{\link[base]{rep}}.
 #'
-#' @param from numerical; inherited from seq().
-#' @param by numerical; inherited from seq().
+#' @param from numerical; inherited from \code{\link[base]{seq}}.
+#' @param by numerical; inherited from \code{\link[base]{seq}}.
 #' @param n.rep numerical; number of time the sequence should be repeated.
-#' @param length.seq numerical; inherited from seq(..., length.out = length.seq).
+#'              Inherited from \code{\link[base]{rep}}.
+#' @param length.seq numerical; inherited from \code{seq(..., length.out = length.seq)}.
 #' @param rep.seq logical; instead of repeating a sequence, (1,2,3,1,2,3),
-#'                sequence a repetition (1,1,2,2,3,3). Default is rep.seq = F.
+#'                sequence a repetition (1,1,2,2,3,3). Default is \code{rep.seq = F}.
 #'
 #' @return a numerical sequence
 #'
 #' @seealso \code{\link[base]{seq}} \code{\link[base]{rep}}
 #'
 #' @keywords internal
-#'
+#' @exportS3Method NULL
 seq.rep <- function(from, by, n.rep, length.seq, rep.seq = F) {
 
   if (rep.seq == F) {
