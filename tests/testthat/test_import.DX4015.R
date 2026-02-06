0a9da84a50c7666f74ac3a05263ca8498ab80f29
emilboegh <emil.boegh@gmail.com>
2026-02-06 20:22:44 +0100

    Changed comments formatting and fixed some language problems.

--- tests/testthat/test_import.DX4015.R
+++ tests/testthat/test_import.DX4015.R
@@ -1,11 +1,23 @@
-library(testthat)
-library(tidyverse)
-library(conflicted)
+# =========================================================================== #
+#                              Import Libraries                               #
+# =========================================================================== #
+library(testthat)   # For testing
+library(tidyverse)  # For piping and data manipulation
+library(conflicted) # For forcing conflict to be errors
 
+# =========================================================================== #
+#                        Source the function to test                          #
+# =========================================================================== #
 source("./R/import.DX4015.R")
 
+# =========================================================================== #
+#                         Create input for the tests                          #
+# =========================================================================== #
 # Test input file:
 inputfile <- "./inst/extdata/DX4015/DX4015.TXT"
+
+# Create a temporary input file with no data
+no.data.inputfile <- tempfile(fileext = ".TXT")
 
 # Create a temporary input file with a different date format based on the
 # test input file:
@@ -27,11 +39,15 @@ read.delim(inputfile, colClasses = "character") %>%
   ) %>%
   write.table(file = faulty.inputfile)
 
+# =========================================================================== #
+#                                Run the tests                                #
+# =========================================================================== #
+
 test_that(
-  desc = "import.DX4015() works correctly",
+  desc = "Test that import.DX4015() works correctly",
   code = {
-    # Test that error handling works with wrong filename
-    expect_warning(import.DX4015("hat"))
+    # Test that error handling works empty data file
+    expect_warning(import.DX4015(no.data.inputfile))
 
     # Test that error handling works with wrong date format
     expect_warning(import.DX4015(faulty.date.inputfile))
